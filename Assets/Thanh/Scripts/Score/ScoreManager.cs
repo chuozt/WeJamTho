cfc3c96fee4de89b8b6b108236aa82845d4a19c9
h3lpmeimdead <vietuc.2004@gmail.com>
2024-06-09 16:50:40 +0700

    a

--- Assets/Thanh/Scripts/Score/ScoreManager.cs
+++ Assets/Thanh/Scripts/Score/ScoreManager.cs
@@ -8,18 +8,10 @@ using UnityEngine.Events;
 public class ScoreManager : Singleton<ScoreManager>
 {
 
-    [SerializeField] List<UnitHouse> houses = new List<UnitHouse>();
-
-
     int points = 0;
     [SerializeField] public TMP_Text scoreText;
-    //private UnitSlot[,] unitSlots;
-    public string text;
 
-    void Start()
-    {
-        //unitSlots = UnitManager.Instance.unitSlots;
-    }
+
     void OnEnable()
     {
         UnitManager.onEndMove += OnNewTurn;
@@ -32,28 +24,11 @@ public class ScoreManager : Singleton<ScoreManager>
 
     void OnNewTurn()
     {
-        AddScore(text);
-        Debug.Log(text);
-        //DestroyHouse();
+        AddScore();
     }
 
-
-
-    void AddScore(string text)
+    void AddScore()
     {
-        Debug.Log(points);
-        //scoreText.text = "";
-        text = scoreText.text;
-        foreach (UnitHouse house in houses)
-        {
-            //points += GetPointsPerLevel(house.HouseLevel);
-            Debug.Log(points);
-            //scoreText.text = text;
-            //points.ToString(text + points);
-            text = "Score: " + points;
-        }
-        return;
-
+        scoreText.text = (int.Parse(scoreText.text) + UnitManager.Instance.UpdateScore()).ToString();
     }
-
 }
